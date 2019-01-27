88cb29c650429e494efc428481b57b09d4bf49c0
jonwaterman <47068424+jonwaterman@users.noreply.github.com>
2019-01-27 01:18:40 -0600

    Add files via upload

---
+++ listFace.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const request = require('request');
+
+// Replace <Subscription Key> with your valid subscription key.
+const subscriptionKey = 'fbd46b617b604099b8a85f9f17587013';
+
+// You must use the same location in your REST call as you used to get your
+// subscription keys. For example, if you got your subscription keys from
+// westus, replace "westcentralus" in the URL below with "westus".
+const uriBase = 'https://southcentralus.api.cognitive.microsoft.com/face/v1.0/largefacelists/{largeFaceListId}/persistedfaces';
+
+const imageUrl =
+    'https://pbs.twimg.com/profile_images/1066768154368663552/GqnW1MDG_400x400.jpg';
+
+// Request parameters.
+const params = {
+    'largeFaceListId': 'waterman_tamuhack2',
+    'userData': 'https://pbs.twimg.com/profile_images/1066768154368663552/GqnW1MDG_400x400.jpg',
+    
+};
+
+const options = {
+    uri: uriBase,
+    qs: params,
+    body: '{"url": ' + '"' + imageUrl + '"}',
+    headers: {
+        'Content-Type': 'application/json',
+        'Ocp-Apim-Subscription-Key' : subscriptionKey
+    }
+};
+
+request.post(options, (error, response, body) => {
+  if (error) {
+    console.log('Error: ', error);
+    return;
+  }
+  let jsonResponse = JSON.stringify(JSON.parse(body), null, '  ');
+  console.log('JSON Response\n');
+  console.log(jsonResponse);
+});
